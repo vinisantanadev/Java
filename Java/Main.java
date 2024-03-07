48ef909b1a3b309c889b2e5d664a89d23c86f7cd
Vinicius Santana <82420721+vinisantanadev@users.noreply.github.com>
2024-03-07 20:38:03 -0300

    scanner

--- Java/Main.java
+++ Java/Main.java
@@ -1,6 +1,20 @@
 package Java;
-class Main {
+import java.util.Scanner;
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello fia da puta");
+
+        Scanner pergunta = new Scanner(System.in);
+
+        System.out.println("do zero para o nada:"); //pergunta
+        String resposta = pergunta.nextLine(); // pega a resposta
+
+
+        System.out.println("do zero para o nada, " + resposta + "!"); // printa a resposta
+
+        System.out.println("Quantos anos nós temos?");
+        float resposta2 = pergunta.nextFloat(); // pega a resposta
+
+        System.out.println("Esta é a nossa idade: " + resposta2 + " anos!"); //
+
     }
 }
